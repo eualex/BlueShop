a4b95b4c61ad6e66ccf9411e30c25e446ae78cf2
alex08g <allex.araujo088@gmail.com>
2020-10-19 16:12:33 -0300

    .

---
+++ frontend/src/pages/Product/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Product: React.FC = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
+
+export default Product;
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/SingInSingUp';
 import Main from './pages/Main';
+import Product from './pages/Product';
 
 const Routes: React.FC = () => {
   return (
@@ -11,7 +12,8 @@ const Routes: React.FC = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
-        <Route path="/Main" component={Main} />
+        <Route path="/main" component={Main} />
+        <Route path="/product" component={Product} />
       </Switch>
     </BrowserRouter>
   )
